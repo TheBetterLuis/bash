fdad75ef47a2b768f841b492cc62b75fc119c794
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2024-11-15 06:56:09 -0400

    bash is interesting, gonna add shortcuts to each one

---
+++ paths.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# this is just a file to save the information
+# path shortcuts
+bashcode="/media/terakek/repos/bash"
+
---
+++ varStart.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+myname="Luis"
+myage="28"
+
+echo "hello , my name is $myname"
+echo "I'm $myage years old."
+
+word="fun"
+now=$(date)
+
+echo "Linux is $word"
+echo "video games are $word"
+echo "sunny days are $word"
+echo "the system time and date is $now"
+date
+
+
+name="Luis Romero"
+echo "hello $name"
+echo "the system time and date is"
+echo $now
+echo "your username is: $USER"
