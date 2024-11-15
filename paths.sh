f5ee893057ab6038c8b01931e26a9f909a3942c0
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2024-11-15 08:44:46 -0400

    lel bajej

--- paths.sh
+++ paths.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 # this is just a file to save the information
 # path shortcuts
-repos="/media/terakek/repos"
-bashcode="/media/terakek/repos/bash"
 
-#lexpin prof repo
+#utils
+pyenv="/media/terakek/Coding/python/python-env/bin/activate"
+
+# path shortcuts
+repos="/media/terakek/repos"
+python="/media/terakek/Coding/python/"
+codewars="/media/terakek/repos/codewars"
+configs="/media/terakek/repos/configs"
+testing="/media/terakek/repos/Testing-Grounds"
+gitv="/media/terakek/Coding/GIT-VSC"
+cbeg="/media/terakek/Coding/GIT-VSC/C"
+cadv="/media/terakek/Coding/GIT-VSC/C/advanced"
+sql="/media/terakek/Coding/GIT-VSC/sql"
+flask="/media/terakek/Coding/python/flaskk"
+
+
+playwii="/media/terakek/PLAYWII"
+ddr="/media/terakek/ddr"
+
+bashcode="/media/terakek/repos/bash"
 lexpincode="/media/terakek/repos/18CPW"
+
+#xampp
+xampp="sudo /opt/lampp/manager-linux-x64.run"
+
+#sql
+sqltraining="mariadb -u betterluis -p training_db"
