0e8bcd9aa334833fbbed3a8c36dc3ca112ed14c8
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2024-11-19 19:04:59 -0400

    telegram script fix

--- telegram.sh
+++ telegram.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-nohup  ~/Downloads/Telegram/Telegram >/dev/null &
+#common alerts go with 2222
+
+# Check if Telegram is already running
+if pgrep -x "Telegram" > /dev/null; then
+  notify-send "Telegram" "Telegram is already runnning." -r 2222 -i ~/Pictures/icons/alert-white-32.png
+else
+  # Run Telegram with output suppressed and in the background
+  nohup ~/Downloads/Telegram/Telegram >/dev/null 2>&1 &
+fi
+
+#nohup  ~/Downloads/Telegram/Telegram >/dev/null &
