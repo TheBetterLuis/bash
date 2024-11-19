b400a372eb843fc793415caa06bdd129d2126216
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2024-11-18 22:45:49 -0400

    mictoggle finished

--- mictoggle.sh
+++ mictoggle.sh
@@ -3,10 +3,10 @@
 micValue=$(cat ~/mic_value.txt)
 
 if [[ $micValue -eq 0 ]]; then
-  notify-send "Mic" "on" -r 9849
+  notify-send "Mic (this doesn't mute mic)" "monitoring turned on" -r 9849 -i ~/Pictures/icons/mic-unmute-white-32.png
   micValue=1  
 else
-  notify-send "Mic" "off" -r 9849
+  notify-send "Mic (this doesn't mute mic)" "monitoring turned off" -r 9849 -i ~/Pictures/icons/mic-mute-red-32.png
   micValue=0 
 fi
 
