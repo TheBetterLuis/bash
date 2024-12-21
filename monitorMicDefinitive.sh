68a2c06df344d8f48defd2e16d34d54cd7de7057
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2024-12-21 02:24:06 -0400

    monitor mic, surely the last one

---
+++ monitorMicDefinitive.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Define the file paths for mic loopback index and mic value
+micLoopbackFile=~/mic_loopback_index.txt
+micValueFile=~/mic_value.txt
+
+# Function to load the microphone loopback module
+load_mic_loopback() {
+  mic_loopback_index=$(pactl load-module module-loopback source=alsa_input.usb-0c76_USB_PnP_Audio_Device-00.mono-fallback sink=monitor_sink)
+  echo $mic_loopback_index > $micLoopbackFile
+  notify-send "Mic (this doesn't mute mic)" "monitoring turned on" -r 9849 -i ~/Pictures/icons/mic-unmute-white-32.png
+}
+
+# Function to unload the microphone loopback module
+unload_mic_loopback() {
+  if [[ -f $micLoopbackFile ]]; then
+    mic_loopback_index=$(cat $micLoopbackFile)
+    pactl unload-module $mic_loopback_index
+    rm $micLoopbackFile
+  fi
+  notify-send "Mic (this doesn't mute mic)" "monitoring turned off" -r 9849 -i ~/Pictures/icons/mic-mute-red-32.png
+}
+
+
+# Read the current mic value
+micValue=$(cat $micValueFile)
+
+ #Toggle the microphone loopback state based on the current mic value
+if [[ $micValue -eq 0 ]]; then
+  load_mic_loopback
+  micValue=1
+else
+  unload_mic_loopback
+  micValue=0
+fi
+
+# Save the new mic value
+echo $micValue > $micValueFile
+
