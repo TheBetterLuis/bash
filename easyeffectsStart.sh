d881e09ecf348183bd00013bd7210e6c87f37410
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2025-01-15 00:36:27 -0400

    added script to open easy effects in tab 8 of dwm

---
+++ easyeffectsStart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+xdotool key Alt+8
+sleep 0.1  # Adjust sleep time as needed
+xdotool key Alt+p
+sleep 0.1
+xdotool type "easyeffects"
+sleep 0.1
+xdotool key Return
+sleep 1
+xdotool key Alt+1 
+
