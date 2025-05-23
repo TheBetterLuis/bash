19bb84c78c953bb4f783571fb65859790923fe4e
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2025-05-23 05:11:08 -0400

    shortcut to open chatterino with specific channel

--- launcher.sh
+++ launcher.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # List of available scripts
-options="check twitch stream\nSystem Actions\nCustom Command"
+options="mpv-twitch\nonline-streamer-twitch\nchat-twitch"
 
 # Use dmenu to select a script
 selection=$(echo -e "$options" | dmenu -i -p "Select a script:")
 
 # Execute the chosen script
 case "$selection" in
-    "check twitch stream") $path_bash/checkTwitch.sh ;;  # The script you created earlier
-    "System Actions") ./system_actions.sh ;;  # Another dmenu-based script
-    "Custom Command") ./custom_command.sh ;;  # Any other script you want
+# open mpv with stream
+    "mpv-twitch") $path_bash/mpvTwitch.sh ;;  
+    # check if streamer is live
+    "online-streamer-twitch") $path_bash/checkTwitch.sh ;;  
+    # chatterino twitch 
+    "chat-twitch") $path_bash/chatterino.sh ;;  
 esac
 
