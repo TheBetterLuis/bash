a06efaef8db2f70f4b3a435f10c3489c998e4e81
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2025-05-26 15:25:18 -0400

    lel

--- mpvCommands/toggleRepeat.sh
+++ mpvCommands/toggleRepeat.sh
@@ -2,6 +2,7 @@
 
 repeat=$(echo '{ "command": ["get_property", "loop-file"] }' | socat - /tmp/mpvsocket | jq -r '.data')
 filename=$(echo '{ "command": ["get_property", "filename"] }' | socat - /tmp/mpvsocket | jq -r '.data')
+paused=$(echo '{ "command": ["get_property", "pause"] }' | socat - /tmp/mpvsocket | jq -r '.data')
 
 if [ "$repeat" == "inf" ]; then
     status_message=""
@@ -12,6 +13,13 @@ else
     echo '{ "command": ["set_property", "loop-file", "yes"] }' | socat - /tmp/mpvsocket
     icon=~/Pictures/icons/repeat-one-white-32.png
 fi
-notify-send "MPV|$status_message" "$filename" -t 1500 -r 1111 -i $icon
+
+if [ "$paused" == "true" ]; then
+    playing="Paused"
+else
+    playing="Playing"
+fi
+
+notify-send "MPV|$playing|$status_message" "$filename" -t 1500 -r 1111 -i $icon
 
 
