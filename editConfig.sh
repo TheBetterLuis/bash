b0e4c3c7d329a47259d1fec99c8bf2884c98cc53
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2025-05-28 00:38:40 -0400

    added dwm slstatus dmenu st configurations to edit conf script

--- editConfig.sh
+++ editConfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of available scripts
-options="vim\nmpv-input\nmpv conf\nsxhkd\nxbindkeys\ndunst\nbash\nzsh\nnewsboat\ntmux\nxinitrc\nxbindkeys\nneomutt"
+options="vim\nmpv-input\nmpv conf\nsxhkd\nxbindkeys\ndunst\nbash\nzsh\nnewsboat\ntmux\nxinitrc\nxbindkeys\nneomutt\ndwm\nst\nslstatus\ndmenu"
 path="/media/terakek/repos/configs"
 # Use dmenu to select a script
 selection=$(echo -e "$options" | dmenu -i -p "Select a script:")
@@ -21,5 +21,9 @@ case "$selection" in
     "xinitrc") st -e vim $path/xinit/.xinitrc > /dev/null 2>&1 ;;  
     "xbindkeys") st -e vim $path/xinit/.xbindkeysrc > /dev/null 2>&1 ;;  
     "neomutt") st -e vim $path/mutt/muttrc > /dev/null 2>&1 ;;  
+    "dwm") st -e vim $path/dwm-flexi/config.h > /dev/null 2>&1 ;;  
+    "st") st -e vim $path/dwm-flexi/st/config.h > /dev/null 2>&1 ;;  
+    "slstatus") st -e vim $path/dwm-flexi/slstatus/config.h > /dev/null 2>&1 ;;  
+    "dmenu") st -e vim $path/dwm-flexi/dmenu/config.h > /dev/null 2>&1 ;;  
 esac
 
